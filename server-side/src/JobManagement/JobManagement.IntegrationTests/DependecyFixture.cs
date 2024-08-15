cf0182e437f8802ca35818e964fb83007cfc8971
markor33 <markorpc1@gmail.com>
2024-08-15 22:22:05 +0200

    JobManagement service integration tests

---
+++ server-side/src/JobManagement/JobManagement.IntegrationTests/DependecyFixture.cs
@@ -0,0 +1,62 @@
+﻿using Amazon.EventBridge;
+using JobManagement.Domain.AggregatesModel.JobAggregate;
+using JobManagement.Domain.AggregatesModel.JobAggregate.Entities;
+using JobManagement.Domain.AggregatesModel.JobAggregate.Enums;
+using JobManagement.Domain.AggregatesModel.JobAggregate.ValueObjects;
+using JobManagement.Domain.Repositories;
+using Moq;
+
+namespace JobManagement.IntegrationTests;
+
+public class DependecyFixture
+{
+    private readonly Mock<IProfessionRepository> _professionRepository = new();
+    private readonly Mock<IJobRepository> _jobRepository = new();
+    private readonly Mock<IAmazonEventBridge> _evenBridge = new();
+
+    public static readonly Guid BlankJobId = Guid.NewGuid();
+    public static readonly Guid JobId = Guid.NewGuid();
+    public static readonly Guid ProposalId = Guid.NewGuid();
+    public static Guid ContractId = Guid.NewGuid();
+    public static readonly Guid FreelancerId = Guid.NewGuid();
+    public static readonly Guid ClientId = Guid.NewGuid();
+    public static readonly Guid ProfessionId = Guid.NewGuid();
+    public static readonly int LanguageId = 1;
+
+    public IJobRepository JobRepository => _jobRepository.Object;
+    public IProfessionRepository ProfessionRepository => _professionRepository.Object;
+    public IAmazonEventBridge EventBridge => _evenBridge.Object;
+
+    public DependecyFixture()
+    {
+        _jobRepository.Setup(x => x.GetByIdAsync(JobId)).ReturnsAsync(GetTestJob());
+        _jobRepository.Setup(x => x.GetByIdAsync(BlankJobId)).ReturnsAsync(GetTestBlankJob());
+        _professionRepository.Setup(x => x.GetByIdAsync(ProfessionId)).ReturnsAsync(GetTestProfession());
+    }
+
+    public static Job GetTestJob()
+    {
+        var job = Job.Create(ClientId, "Title", "Desc", ExperienceLevel.JUNIOR, new Payment(100, "USD", PaymentType.FIXED_RATE), GetTestProfession(), [], []);
+        
+        var proposal = new Proposal(ProposalId, FreelancerId, "Text", new Payment(100, "USD", PaymentType.FIXED_RATE), ProposalStatus.CLIENT_APPROVED, [], DateTime.UtcNow);
+        job.AddProposal(proposal);
+
+        var proposal1 = new Proposal(Guid.NewGuid(), Guid.NewGuid(), "Text", new Payment(100, "USD", PaymentType.FIXED_RATE), ProposalStatus.CLIENT_APPROVED, [], DateTime.UtcNow);
+        job.AddProposal(proposal1);
+        var res = job.MakeContract(proposal1.Id);
+        ContractId = res.Value.Id;
+
+        return job;
+    }
+
+    public static Job GetTestBlankJob()
+    {
+        var job = Job.Create(ClientId, "Title", "Desc", ExperienceLevel.JUNIOR, new Payment(100, "USD", PaymentType.FIXED_RATE), GetTestProfession(), [], []);
+
+        return job;
+    }
+
+    public static Profession GetTestProfession()
+        => new Profession(ProfessionId, "name", "desc", new List<Skill>());
+
+}
---
+++ server-side/src/JobManagement/JobManagement.IntegrationTests/JobScenario.cs
@@ -0,0 +1,83 @@
+﻿using JobManagement.Application.Commands.JobCommands;
+using JobManagement.Domain.AggregatesModel.JobAggregate.Enums;
+using JobManagement.Domain.AggregatesModel.JobAggregate.ValueObjects;
+using Shouldly;
+
+namespace JobManagement.IntegrationTests;
+
+public class JobScenario : BaseIntegrationTest
+{
+    public JobScenario(DependecyFixture dependecyFixture) : base(dependecyFixture) { }
+
+    [Fact]
+    public async Task Create_Job_ReturnsOk()
+    {
+        var commandHandler = new CreateJobCommandHandler(fixture.JobRepository, fixture.ProfessionRepository);
+
+        var result = await commandHandler.Handle(GetTestCreateJobCommand(), CancellationToken.None);
+
+        result.IsSuccess.ShouldBeTrue();
+    }
+
+    [Fact]
+    public async Task Update_Job_ReturnsOk()
+    {
+        var commandHandler = new UpdateJobCommandHandler(fixture.JobRepository, fixture.ProfessionRepository);
+
+        var result = await commandHandler.Handle(GetTestUpdateJobCommand(), CancellationToken.None);
+
+        result.IsSuccess.ShouldBeTrue();
+    }
+
+    [Fact]
+    public async Task Job_Done_ReturnsOk()
+    {
+        var commandHandler = new JobDoneCommandHandler(fixture.JobRepository);
+
+        var result = await commandHandler.Handle(GetTestJobDoneCommand(DependecyFixture.BlankJobId), CancellationToken.None);
+
+        result.IsSuccess.ShouldBeTrue();
+    }
+
+    [Fact]
+    public async Task Job_Done_HasActiveContracts_ReturnsBadRequest()
+    {
+        var commandHandler = new JobDoneCommandHandler(fixture.JobRepository);
+
+        var result = await commandHandler.Handle(GetTestJobDoneCommand(DependecyFixture.JobId), CancellationToken.None);
+
+        result.IsSuccess.ShouldBeFalse();
+    }
+
+    [Fact]
+    public async Task Job_Delete_ReturnsOk()
+    {
+        var commandHandler = new DeleteJobCommandHandler(fixture.JobRepository);
+
+        var result = await commandHandler.Handle(GetTestDeleteJobCommand(DependecyFixture.BlankJobId), CancellationToken.None);
+
+        result.IsSuccess.ShouldBeTrue();
+    }
+
+    [Fact]
+    public async Task Job_Delete_HasContracts_ReturnsBadRequest()
+    {
+        var commandHandler = new DeleteJobCommandHandler(fixture.JobRepository);
+
+        var result = await commandHandler.Handle(GetTestDeleteJobCommand(DependecyFixture.JobId), CancellationToken.None);
+
+        result.IsSuccess.ShouldBeFalse();
+    }
+
+    private static CreateJobCommand GetTestCreateJobCommand()
+        => new(Guid.NewGuid(), "Title", "Desc", ExperienceLevel.JUNIOR, new Payment(100, "USD", PaymentType.FIXED_RATE), [], DependecyFixture.ProfessionId, []);
+
+    private static UpdateJobCommand GetTestUpdateJobCommand()
+        => new(DependecyFixture.JobId, "Title", "Desc", ExperienceLevel.JUNIOR, new Payment(100, "USD", PaymentType.FIXED_RATE), [], DependecyFixture.ProfessionId, []);
+
+    private static JobDoneCommand GetTestJobDoneCommand(Guid jobId)
+        => new(jobId);
+
+    private static DeleteJobCommand GetTestDeleteJobCommand(Guid jobId)
+        => new(jobId);
+}
