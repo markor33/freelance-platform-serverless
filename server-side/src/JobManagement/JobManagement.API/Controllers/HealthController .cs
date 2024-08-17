a91f039f032d9e79dec86954642caee05a728bcd
markor33 <markorpc1@gmail.com>
2024-08-17 23:41:45 +0200

    Health controller

---
+++ server-side/src/JobManagement/JobManagement.API/Controllers/HealthController .cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobManagement.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HealthController : ControllerBase
+{
+    [HttpGet("health")]
+    public IActionResult HealthCheck()
+    {
+        return Ok("Healthy");
+    }
+}
